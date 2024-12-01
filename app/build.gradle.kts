622d557be9e7a35f9842f34b0b4069d1a8b0c409
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2024-12-02 00:18:14 +0100

    ktlint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,70 +1,48 @@
 import com.google.protobuf.gradle.id
-import me.champeau.jmh.JMHTask
 
 plugins {
-    application
-    id("com.google.protobuf") version "0.9.4"
-    id("me.champeau.jmh") version "0.7.2"
-    id("com.diffplug.spotless") version "7.0.0.BETA4"
-    id("io.freefair.lombok") version "8.11"
+  application
+  id("com.google.protobuf") version "0.9.4"
+  id("me.champeau.jmh") version "0.7.2"
+  id("com.diffplug.spotless") version "7.0.0.BETA4"
+  id("io.freefair.lombok") version "8.11"
 }
 
 val grpcVersion = "1.68.2"
 val protobufVersion = "4.29.0"
 val protocVersion = protobufVersion
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
-    implementation("com.google.protobuf:protobuf-java-util:$protocVersion")
-    implementation("io.grpc:grpc-protobuf:$grpcVersion")
-    implementation("io.grpc:grpc-services:$grpcVersion")
-    implementation("io.grpc:grpc-stub:$grpcVersion")
-    implementation("io.grpc:grpc-netty-shaded:$grpcVersion")
-    compileOnly("org.apache.tomcat:annotations-api:6.0.53")
+  implementation("com.google.protobuf:protobuf-java-util:$protocVersion")
+  implementation("io.grpc:grpc-protobuf:$grpcVersion")
+  implementation("io.grpc:grpc-services:$grpcVersion")
+  implementation("io.grpc:grpc-stub:$grpcVersion")
+  implementation("io.grpc:grpc-netty-shaded:$grpcVersion")
+  compileOnly("org.apache.tomcat:annotations-api:6.0.53")
 
-    testImplementation(libs.junit.jupiter)
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  testImplementation(libs.junit.jupiter)
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 protobuf {
-    protoc {
-        artifact = "com.google.protobuf:protoc:$protocVersion"
-    }
-    plugins {
-        id("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
-        }
-    }
-    generateProtoTasks {
-        ofSourceSet(sourceSets.main.name).forEach {
-            it.plugins {
-                id("grpc") { }
-            }
-        }
-    }
+  protoc { artifact = "com.google.protobuf:protoc:$protocVersion" }
+  plugins { id("grpc") { artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion" } }
+  generateProtoTasks { ofSourceSet(sourceSets.main.name).forEach { it.plugins { id("grpc") {} } } }
 }
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
+java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
-application {
-    mainClass = "com.fandreuz.grpc.uds.Server"
-}
+application { mainClass = "com.fandreuz.grpc.uds.Server" }
 
-tasks.named<Test>("test") {
-    useJUnitPlatform()
-}
+tasks.named<Test>("test") { useJUnitPlatform() }
 
 spotless {
-    java {
-        removeUnusedImports()
-        indentWithSpaces()
-        palantirJavaFormat()
-    }
+  java {
+    removeUnusedImports()
+    indentWithSpaces()
+    palantirJavaFormat()
+  }
+  kotlinGradle { ktfmt() }
 }
