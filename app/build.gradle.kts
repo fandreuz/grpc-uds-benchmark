d54ad09381469470ec73fb2bf65138e912e9f2e9
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2024-12-01 13:30:33 +0100

    grpc/proto stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,46 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     application
+    id("com.google.protobuf") version "0.9.4"
 }
+
+val grpcVersion = "1.68.2"
+val protobufVersion = "4.29.0"
+val protocVersion = protobufVersion
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("com.google.protobuf:protobuf-java-util:$protocVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("io.grpc:grpc-services:$grpcVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    runtimeOnly("io.grpc:grpc-netty-shaded:$grpcVersion")
+    compileOnly("org.apache.tomcat:annotations-api:6.0.53")
+
     testImplementation(libs.junit.jupiter)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protocVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet(sourceSets.main.name).forEach {
+            it.plugins {
+                id("grpc") { }
+            }
+        }
+    }
 }
 
 java {
--- app/src/main/java/com/fandreuz/grpc/uds/benchmark/Main.java
+++ app/src/main/java/com/fandreuz/grpc/uds/benchmark/Main.java
@@ -1,9 +1,12 @@
 package com.fandreuz.grpc.uds.benchmark;
 
+import com.fandreuz.grpc.uds.benchmark.echo.helloworld.GreeterGrpc;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("TODO");
+
     }
 
 }
