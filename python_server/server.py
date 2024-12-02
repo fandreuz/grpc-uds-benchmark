258203d393d07b37dc5b25ad57526438b237a4ce
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2024-12-02 16:58:01 +0100

    fix response build

--- python_server/server.py
+++ python_server/server.py
@@ -9,7 +9,7 @@ import helloworld_pb2_grpc
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
     def SayHello(self, request: helloworld_pb2.HelloRequest, context):
-        return helloworld_pb2.HelloReply(request.payload)
+        return helloworld_pb2.HelloReply(payload=request.payload)
 
 
 if __name__ == "__main__":
