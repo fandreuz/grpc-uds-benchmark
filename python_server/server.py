d81f1f5f57c9405cf5a3abe624489d468b1ae9cb
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2024-12-03 09:13:19 +0100

    set max grpc size

--- python_server/server.py
+++ python_server/server.py
@@ -1,10 +1,13 @@
 import sys
 from concurrent import futures
+from typing import Final
 
 import grpc
 
 import helloworld_pb2
 import helloworld_pb2_grpc
+
+grpc_max_bytes: Final = 209715200
 
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
@@ -13,7 +16,13 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
 
 if __name__ == "__main__":
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10),
+        options=[
+            ("grpc.max_send_message_length", grpc_max_bytes),
+            ("grpc.max_recieve_message_length", grpc_max_bytes),
+        ],
+    )
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port(sys.argv[1])
     server.start()
